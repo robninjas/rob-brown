4d9e1fd2306ecb28a887aa5eb3510bc9ac0234fa
Alec Chen <robninjas@proton.me>
2023-06-28 20:01:28 -0400

    WORKMMS FINISHEDDD

---
+++ WORMSsSsSsSsSs/Assets/Scripts/TurretLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretLook : MonoBehaviour
+{
+    public Transform player;
+    private LaserSwitch laserSwitch;
+    private GameObject laser;
+
+    private void Start()
+    {
+        laserSwitch = GameObject.Find("LaserSwitch").GetComponent<LaserSwitch>();
+        laser = transform.Find("Laser").gameObject;
+    }
+
+    void Update() 
+    {
+        if (laserSwitch.arelazarsoff)
+        {
+            laser.SetActive(false);
+            return;
+        }
+
+        transform.LookAt(player);
+    }
+}
